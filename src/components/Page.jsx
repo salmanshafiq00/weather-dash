d35dbd2e1b7980f062fef32bcf4343524fda78dc
salman <salmanshafiq00@gmail.com>
2026-02-27 00:33:51 +0600

    Add WeatherBoard component to display weather information

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,10 +1,16 @@
 import Header from "./header/Header";
 import Layout from "./Layout";
+import WeatherBoard from "./weather/WeatherBoard";
 
 function Page() {
   return (
     <Layout>
       <Header />
+      <main>
+        <section>
+          <WeatherBoard />
+        </section>
+      </main>
     </Layout>
   );
 };
